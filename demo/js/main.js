51204fca1cea1c8a9038c3fad449f2ce63230d4c
khanhvudo72 <vudhk@magik.vn>
2019-01-08 10:11:44 +0700

    edit something for web demo

--- demo/js/main.js
+++ demo/js/main.js
@@ -10,4 +10,26 @@ $(function () {
 	$('.fullscreen').css('height', window_height);
 	$('.fitscreen').css('height', fitscreen);
 
+	$('#gt-clouds').on('click', function () {
+		alert("This algorithm is demoed on MATLAB.")
+	});
+
+	$("#gt-lrcn").on('click', function () {
+		setTimeout(function () {
+			$('html,body').animate({
+				scrollTop: $("#demo-area").offset().top
+			}, 'slow');
+		}, 0);
+	});
+
+	$('#in-video').on('change', function () {
+		if (this.files.length <= 0)
+			return;
+		var inVideo = this.files[0];
+		var videoUrl = URL.createObjectURL(inVideo);
+		setTimeout(function () {
+			$('#in-video-src').attr('src', 'https://www.youtube.com/embed/F9Bo89m2f6g');
+		}, 0);
+	})
+
 });
